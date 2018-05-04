d215bd35d6548699852ec5809182c2c7f92d51d5
DevinNorgarb <dnorgarb@gmail.com>
2018-05-04 23:41:47 +0200

    Youtube dl

--- src/Http/DownloadController.php
+++ src/Http/DownloadController.php
@@ -8,6 +8,9 @@ class DownloadController extends Controller
 {
     public function download()
     {
+        $yt = new YouTubeDownloader();
+        $links = $yt->getDownloadLinks("https://www.youtube.com/watch?v=QxsmWxxouIM");
+        dd($links);
         return "This is my todo list from the controller";
     }
 }
